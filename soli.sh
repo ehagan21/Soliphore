05aaa20b1565183d356bee611f61af73c2fa278d
Eric Hagan <ehagan21@gmail.com>
2019-05-19 18:12:36 -0400

    Create soli.sh
    File for autostarting kiosk mode on the soliphore project

---
+++ soli.sh
@@ -0,0 +1,8 @@
+'#!/bin/bash
+
+xset s noblank
+xset s off
+xset -dpms
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+/usr/bin/chromium-browser --noerrordialogs --disable-infobrs --kiosk http://soliphore.herokuapp.com
